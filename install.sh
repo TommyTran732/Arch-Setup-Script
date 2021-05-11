158f58f66c2bd510e3357fb2d1682ecf81ee3d8a
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-05-11 06:36:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -214,6 +214,27 @@ kernel.yama.ptrace_scope = 3
 kernel.unprivileged_userns_clone = 1
 EOF
 
+# Randomize Mac Address
+bash -c 'cat > /mnt/etc/NetworkManager/conf.d/00-macrandomize.conf' <<-'EOF'
+[device]
+wifi.scan-rand-mac-address=yes
+[connection]
+wifi.cloned-mac-address=random
+ethernet.cloned-mac-address=random
+connection.stable-id=${CONNECTION}/${BOOT}
+EOF
+
+chmod 700 /mnt/etc/NetworkManager/conf.d/00-macrandomize.conf
+
+# Disable Connectivity Check 
+bash -c 'cat > /mnt/etc/NetworkManager/conf.d/20-connectivity.conf' <<-'EOF'
+[connectivity]
+uri=http://www.archlinux.org/check_network_status.txt
+interval=0
+EOF
+
+chmod 700 /mnt/etc/NetworkManager/conf.d/20-connectivity.conf
+
 # Configuring the system.    
 arch-chroot /mnt /bin/bash -e <<EOF
     
@@ -252,9 +273,6 @@ arch-chroot /mnt /bin/bash -e <<EOF
     echo "Creating GRUB config file."
     grub-mkconfig -o /boot/grub/grub.cfg &>/dev/null
     pacman -S --noconfirm snap-pac
-    
-    #Remove unnecessary permissions
-    chmod o-w /var/cache
 EOF
 
 # Setting root password.
