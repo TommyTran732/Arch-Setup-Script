c347c9f87a768ec641537e29e8d2da77adcde7c5
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2021-07-25 07:47:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -340,5 +340,19 @@ sed -i 's/022/077/g' /mnt/etc/profile
 echo "" >> /mnt/etc/bash.bashrc
 echo "umask 077" >> /mnt/etc/bash.bashrc
 
+# Setting up ZRAM
+MEM = $(awk '/^Mem/ {print $2}' <(free -m))
+if [ "${MEM}" -ge "8192" ]; then
+    ZRAMSIZE = 8192
+else 
+    ZRAMSIZE = ${MEM}
+fi 
+
+echo 'zram' > /mnt/etc/modules-load.d/zram.conf
+echo 'options zram num_devices=1' > /mnt/etc/modprobe.d/zram.conf
+echo 'KERNEL=="zram0", ATTR{disksize}="${ZRAMSIZE}M" RUN="/usr/bin/mkswap /dev/zram0", TAG+="systemd"' > /mnt/etc/udev/rules.d/99-zram.rules
+echo '# ZRAM' >> /mnt/etc/fstab
+echo '/dev/zram0 					none 		swap 		defaults 0 0' >> /mnt/etc/fstab
+
 echo "Done, you may now wish to reboot (further changes can be done by chrooting into /mnt)."
 exit
