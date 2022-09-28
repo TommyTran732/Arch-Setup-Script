6accad464bd3c784eb30f9f03916e589cf12ba34
funk-on-code <113871227+funk-on-code@users.noreply.github.com>
2022-09-28 16:23:11 +0000

    Fixes ESP Generation from Archlinux 2022.07+ (#16)
    Fixes ESP filesystem generation on Live Archlinux ISOs from July 2022 to September (fixes both "Sector Not Allocated" error from mkfs.fat and the grub-install command since the changes to grub-install introduced in the latest update.
    
    Signed-off-by: funk-on-code <113871227+funk-on-code@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -31,20 +31,20 @@ pacman -S --noconfirm curl
 # Selecting the kernel flavor to install.
 kernel_selector () {
     echo "List of kernels:"
-    echo "1) Hardened — A security-focused Linux kernel."
-    echo "2) Longterm — Long-term support (LTS) Linux kernel and modules."
-    echo "3) Zen Kernel — Optimized for desktop usage."
-    echo "4) Stable — Vanilla Linux kernel and modules, with a few patches applied."
+    echo "1) Stable — Vanilla Linux kernel and modules, with a few patches applied."
+    echo "2) Hardened — A security-focused Linux kernel."
+    echo "3) Longterm — Long-term support (LTS) Linux kernel and modules."
+    echo "4) Zen Kernel — Optimized for desktop usage."
     read -r -p "Insert the number of the corresponding kernel: " choice
     echo "$choice will be installed"
     case $choice in
-        1 ) kernel=linux-hardened
+        1 ) kernel=linux
             ;;
-        2 ) kernel=linux-lts
+        2 ) kernel=linux-hardened
             ;;
-        3 ) kernel=linux-zen
+        3 ) kernel=linux-lts
             ;;
-        4 ) kernel=linux
+        4 ) kernel=linux-zen
             ;;
         * ) echo "You did not enter a valid selection."
             kernel_selector
