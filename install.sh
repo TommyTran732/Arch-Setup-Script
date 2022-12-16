7ff0405c6020c4a303c862dfd050f23f00d777dd
Welteam <8932313+Welteam@users.noreply.github.com>
2022-12-16 20:12:18 +0000

    Fix sed command for sudo access (#21)
    Adapt sed regexp to variations of sudoers file
    
    Signed-off-by: Welteam <8932313+Welteam@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -393,7 +393,7 @@ arch-chroot /mnt chown -R $username:$username /home/${username}/.config
 [ -n "$username" ] && echo "Setting user password for ${username}." && arch-chroot /mnt /bin/passwd "$username"
 
 # Giving wheel user sudo access.
-sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /mnt/etc/sudoers
+sed -i 's/# \(%wheel ALL=(ALL\(:ALL\|\)) ALL\)/\1/g' /mnt/etc/sudoers
 
 # Change audit logging group
 echo "log_group = audit" >> /mnt/etc/audit/auditd.conf
