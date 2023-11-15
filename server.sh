e40b76ec02786f860389f2954faf542524fdd6e8
Tommy <contact@tommytran.io>
2023-11-15 14:24:58 -0700

    Update 990-security-misc path
    Signed-off-by: Tommy <contact@tommytran.io>

--- server.sh
+++ server.sh
@@ -274,8 +274,8 @@ curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/modpr
 chmod 600 /mnt/etc/modprobe.d/*
 
 # Security kernel settings.
-curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc.conf >> /mnt/etc/sysctl.d/30_security-misc.conf
-sed -i 's/kernel.yama.ptrace_scope=2/kernel.yama.ptrace_scope=3/g' /mnt/etc/sysctl.d/30_security-misc.conf
+curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/usr/lib/sysctl.d/990-security-misc.conf >> /mnt/etc/sysctl.d/990-security-misc.conf
+sed -i 's/kernel.yama.ptrace_scope=2/kernel.yama.ptrace_scope=3/g' /mnt/etc/sysctl.d/990-security-misc.conf
 curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_silent-kernel-printk.conf >> /mnt/etc/sysctl.d/30_silent-kernel-printk.conf
 curl https://raw.githubusercontent.com/Kicksecure/security-misc/master/etc/sysctl.d/30_security-misc_kexec-disable.conf >> /mnt/etc/sysctl.d/30_security-misc_kexec-disable.conf
 chmod 600 /mnt/etc/sysctl.d/*
